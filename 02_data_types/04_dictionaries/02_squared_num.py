019a94e263f5cbfed225a00d828d78ae32dd6774
AnkushNikam06 <151992863+AnkushNikam06@users.noreply.github.com>
2025-04-29 18:56:34 +0530

    Create 02_squared_num.py

---
+++ 02_data_types/04_dictionaries/02_squared_num.py
@@ -0,0 +1,7 @@
+# This is a file created to work on the practicals of the course. This is a course based on dictionary index squaring using range. 
+# You can simply create a calculator using this code.
+
+n= int(input("Enter the range value: "))
+squared_num = {n: n**2 for n in range(n)}
+
+print("The square of the range value is: ", squared_num)
