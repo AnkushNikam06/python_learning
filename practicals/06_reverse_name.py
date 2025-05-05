62576a94ac6fc4328809f3ed6101e4faecf9ff1b
AnkushNikam06 <151992863+AnkushNikam06@users.noreply.github.com>
2025-05-05 11:14:46 +0530

    Create 06_reverse_name.py

---
+++ practicals/06_reverse_name.py
@@ -0,0 +1,7 @@
+# Q: Write a python code to reverse a name entered by the user.
+
+fname = str(input("Enter the first name: "))
+lname = str(input("Enter the last name: "))
+
+
+print("The reversed name is: ", lname + " " + fname)
