2ede57309d6e7c3d46479fce19a27f8552233319
AnkushNikam06 <151992863+AnkushNikam06@users.noreply.github.com>
2025-05-15 19:11:53 +0530

    Create 02_function_with_multi_parameters.py

---
+++ practicals/04_functions/02_function_with_multi_parameters.py
@@ -0,0 +1,10 @@
+# Q: Create a function that takes two numbers as parameters and returns their sum
+
+num1 = int(input("Enter the value of num1: "))
+num2 = int(input("Enter the value of num2: "))
+
+def sum(num1, num2):
+    return num1 + num2
+
+result= sum(num1, num2)
+print(result)
