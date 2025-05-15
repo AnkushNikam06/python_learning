6bf507cb07f13a1be47d3f832cf43b944a9fc787
AnkushNikam06 <151992863+AnkushNikam06@users.noreply.github.com>
2025-05-15 19:11:09 +0530

    Create 01_basic_function_syntax.py

---
+++ practicals/04_functions/01_basic_function_syntax.py
@@ -0,0 +1,9 @@
+# Q: Write a function to calculate and return the square of a number
+
+num = int(input("Enter the number: "))
+
+def square_num(num):
+    return num ** 2
+    
+result = square_num(num)
+print(result)
